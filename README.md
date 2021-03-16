8c05ae5ba304862efa9445e54313736f8d86d495
mstefaniak <Mikolaj.Stefaniak@eu.equinix.com>
2021-03-16 18:36:07 +0100

    Initial version of the module

---
+++ README.md
@@ -0,0 +1,122 @@
+# Equinix Network Edge: Cisco CSR SD-WAN edge device
+
+A Terraform module to create Cisco CSR SD-WAN network edge device
+on the Equinix platform.
+
+![Terraform status](https://github.com/equinix/terraform-equinix-csr-sdwan/workflows/Terraform/badge.svg)
+![License](https://img.shields.io/github/license/equinix/terraform-equinix-csr-sdwan)
+
+Supported device modes:
+
+| Management Mode | License mode | Notes |
+|-----------------|--------------|-------|
+| Self managed    | Bring your own license | `license_file` required |
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 0.13.0 |
+| equinix/equinix | >= 1.1.0 |
+
+## Providers
+
+| Name | Version |
+|---------|----------|
+| equinix/equinix | >= 1.1.0 |
+
+## Assumptions
+
+* if `account_number` is not provided, then `Active` account within given metro
+will be used
+* most recent, stable version of a device software for a given `software_package`
+will be used
+* secondary device name will be same as primary with `-secondary` suffix added
+* secondary device notification list will be same as for primary
+* secondary device interface count will be always same as primary interface count
+
+## Example usage
+
+```hcl
+provider equinix {
+  client_id     = var.equinix_client_id
+  client_secret = var.equinix_client_secret
+}
+
+module "csr-sdwan" {
+  source            = "equinix/csr-sdwan/equinix"
+  metro_code        = "SV"
+  platform          = "large"
+  software_package  = "PREMIER"
+  license_file      = "/tmp/csrsdwan-pri.cfg"
+  throughput        = 1
+  throughput_unit   = "Gbps"
+  name              = "tf-tst-csr-sdwan"
+  term_length       = 1
+  notifications     = ["test@test.com"]
+  acl_tempalte_id   = "2e365e34-8f38-46e1-9f57-94b075d5dc09"
+  interface_count   = 24
+  site_id           = "435"
+  system_ip_address = "123.53.66.34"
+  secondary = {
+    enabled           = true
+    metro_code        = "DC"
+    license_file      = "/tmp/csrsdwan-sec.cfg"
+    acl_tempalte_id   = "81a90c41-8a22-4724-997c-bdc07f401387"
+    site_id           = "488"
+    system_ip_address = "84.141.11.5"
+  }
+}
+```
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|----------|
+|metro_code|Two-letter device location's metro code|`string`|`""`|yes|
+|account_number|Billing account number for a device. If not provided, active account for a device metro code will be used|`string`|`0`|no|
+|platform|Device hardware platform flavor: `small`, `medium`, `large`|`string`|`""`|yes|
+|software_package|Device software package: `ADVANTAGE`, `ESSENTIALS`, `PREMIER`|`string`|`""`|yes|
+|license_file|Path to the device license configuration fil|`string`|`""`|yes|
+|throughput|Device license throughput|`number`|`0`|yes|
+|throughput_unit|License throughput unit (`Mbps` or `Gbps`)|`string`|`""`|yes|
+|name|Device name|`string`|`""`|yes|
+|term_length|Term length in months: `1`, `12`, `24`, `36`|`number`|`0`|yes|
+|notifications|List of email addresses that will receive notifications about device|`list(string)`|n/a|yes|
+|site_id|Site identifier|`string`|`""`|yes|
+|system_ip_address|System IP address|`string`|`""`|yes|
+|acl_template_id|Identifier of a network ACL template that will be applied on a device|`string`|`""`|yes|
+|additional_bandwidth|Amount of additional internet bandwidth for a device, in Mbps|`number`|`0`|no|
+|interface_count|Device interface count: either `10` or `24`|`number`|`10`|no|
+|secondary|Map of secondary device attributes in redundant setup|`map`|N/A|no|
+
+Secondary device map attributes:
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|----------|
+|enabled|Value that determines if secondary device shall be created|`bool`|`false`|no|
+|metro_code|Two-letter secondary device location's metro code|`string`|`""`|yes|
+|account_number|Billing account number for a device. If not provided, active account for a device metro code will be used|`string`|`0`|no|
+|license_file|Path to the device license configuration fil|`string`|`""`|yes|
+|site_id|Site identifier|`string`|`""`|yes|
+|system_ip_address|System IP address|`string`|`""`|yes|
+|acl_template_id|Identifier of a network ACL template that will be applied on a secondary device|`string`|`""`|yes|
+|additional_bandwidth|Amount of additional internet bandwidth for a secondary device, in Mbps|`number`|`0`|no|
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+|id|Device identifier|
+|status|Device provisioning status|
+|license_status|Device license status|
+|account_number|Device billing account number|
+|cpu_count|Number of device CPU cores|
+|memory|Amount of device memory|
+|software_version|Device software version|
+|region|Device region|
+|ibx|Device IBX center code|
+|ssh_ip_address|Device SSH interface IP address|
+|ssh_ip_fqdn|Device SSH interface FQDN|
+|interfaces|List of network interfaces present on a device|
+|secondary|Secondary device outputs (same as for primary). Present when secondary device was enabled|
---
+++ variables.tf
@@ -0,0 +1,167 @@
+variable "metro_code" {
+  description = "Device location metro code"
+  type        = string
+  validation {
+    condition     = can(regex("^[A-Z]{2}$", var.metro_code))
+    error_message = "Valid metro code consits of two capital leters, i.e. SV, DC."
+  }
+}
+
+variable "account_number" {
+  description = "Billing account number for a device"
+  type        = string
+  default     = 0
+}
+
+variable "platform" {
+  description = "Device platform flavor that determines number of CPU cores and memory"
+  type        = string
+  validation {
+    condition     = can(regex("^(small|medium|large)$", var.platform))
+    error_message = "One of following platform flavors are supported: small, medium, large."
+  }
+}
+
+variable "software_package" {
+  description = "Device software package"
+  type        = string
+  validation {
+    condition     = can(regex("^(ADVANTAGE|ESSENTIALS|PREMIER)$", var.software_package))
+    error_message = "One of following software packages are supported: ADVANTAGE, ESSENTIALS, PREMIER."
+  }
+}
+
+variable "license_file" {
+  description = "Path to the device license configuration file"
+  type        = string
+  validation {
+    condition     = length(var.license_file) > 0
+    error_message = "Device license configuration path has be an non empty string."
+  }
+}
+
+variable "throughput" {
+  description = "Device license throughput"
+  type        = number
+  validation {
+    condition     = var.throughput > 0
+    error_message = "Device license throughput has to be positive number."
+  }
+}
+
+variable "throughput_unit" {
+  description = "License throughput unit (Mbps or Gbps)"
+  type        = string
+  validation {
+    condition     = can(regex("^(Mbps|Gbps)$", var.throughput_unit))
+    error_message = "One of following throughput units are available: Mbps or Gbps."
+  }
+}
+
+variable "name" {
+  description = "Device name"
+  type        = string
+  validation {
+    condition     = length(var.name) >= 2 && length(var.name) <= 50
+    error_message = "Device name should consist of 2 to 50 characters."
+  }
+}
+
+variable "term_length" {
+  description = "Term length in months"
+  type        = number
+  validation {
+    condition     = can(regex("^(1|12|24|36)$", var.term_length))
+    error_message = "One of following term lengths are available: 1, 12, 24, 36 months."
+  }
+}
+
+variable "notifications" {
+  description = "List of email addresses that will receive device status notifications"
+  type        = list(string)
+  validation {
+    condition     = length(var.notifications) > 0
+    error_message = "Notification list cannot be empty."
+  }
+}
+
+variable "acl_template_id" {
+  description = "Identifier of an ACL template that will be applied on a device"
+  type        = string
+  validation {
+    condition     = length(var.acl_template_id) > 0
+    error_message = "ACL template identifier has to be an non empty string."
+  }
+}
+
+variable "additional_bandwidth" {
+  description = "Additional internet bandwidth for a device"
+  type        = number
+  default     = 0
+  validation {
+    condition     = var.additional_bandwidth == 0 || (var.additional_bandwidth >= 25 && var.additional_bandwidth <= 2001)
+    error_message = "Additional internet bandwidth should be between 25 and 2001 Mbps."
+  }
+}
+
+variable "site_id" {
+  description = "Site identifier"
+  type        = string
+  validation {
+    condition     = length(var.site_id) > 0
+    error_message = "Site identifier has to be an non empty string."
+  }
+}
+
+variable "system_ip_address" {
+  description = "System IP address"
+  type        = string
+  validation {
+    condition     = length(var.system_ip_address) > 0
+    error_message = "System IP address has to be an non empty string."
+  }
+}
+
+variable "interface_count" {
+  description = "Device license throughput"
+  type        = number
+  default     = 0
+  validation {
+    condition     = var.interface_count == 10 || var.interface_count == 24
+    error_message = "Device interface count has to be either 10 or 24."
+  }
+}
+
+variable "secondary" {
+  description = "Secondary device attributes"
+  type        = map(any)
+  default     = { enabled = false }
+  validation {
+    condition     = can(var.secondary.enabled)
+    error_message = "Key 'enabled' has to be defined for secondary device."
+  }
+  validation {
+    condition     = !try(var.secondary.enabled, false) || try(length(var.secondary.license_file) > 0, false)
+    error_message = "Key 'license_file' has to be defined for secondary device. Valid license file path has to be an non empty string."
+  }
+  validation {
+    condition     = !try(var.secondary.enabled, false) || can(regex("^[A-Z]{2}$", var.secondary.metro_code))
+    error_message = "Key 'metro_code' has to be defined for secondary device. Valid metro code consits of two capital leters, i.e. SV, DC."
+  }
+  validation {
+    condition     = !try(var.secondary.enabled, false) || try(length(var.secondary.acl_template_id) > 0, false)
+    error_message = "Key 'acl_template_id' has to be defined for secondary device. Valid ACL template identifier has to be an non empty string."
+  }
+  validation {
+    condition     = !try(var.secondary.enabled, false) || try(var.secondary.additional_bandwidth >= 25 && var.secondary.additional_bandwidth <= 2001, true)
+    error_message = "Key 'additional_bandwidth' has to be between 25 and 2001 Mbps."
+  }
+  validation {
+    condition     = !try(var.secondary.enabled, false) || try(length(var.secondary.site_id) > 0, false)
+    error_message = "Key 'site_id' has to be defined for secondary device. Valid site identifier has to be an non empty string."
+  }
+  validation {
+    condition     = !try(var.secondary.enabled, false) || try(length(var.secondary.system_ip_address) > 0, false)
+    error_message = "Key 'system_ip_address' has to be defined for secondary device. Valid system IP address has to be an non empty string."
+  }
+}
